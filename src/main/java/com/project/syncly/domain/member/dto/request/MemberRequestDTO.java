1c047fdcc58fe80dd89158ad5dd471025ad20379
CHO <imalex12@naver.com>
2025-05-08 04:09:15 +0900

    feat: Login dto추가. 검증
    Login dto추가. 이메일 여부검증, 비밀번호 규정 검증, 닉네임 규정 검증 추가

--- src/main/java/com/project/syncly/domain/member/dto/request/MemberRequestDTO.java
+++ src/main/java/com/project/syncly/domain/member/dto/request/MemberRequestDTO.java
@@ -1,10 +1,19 @@
 package com.project.syncly.domain.member.dto.request;
 
 
+import com.project.syncly.global.validator.annotation.ValidName;
+import com.project.syncly.global.validator.annotation.ValidPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class MemberRequestDTO {
-    public record signUp(
-            String email,
-            String password,
-            String name
+    public record SignUp(
+            @Email @NotBlank String email,
+            @ValidPassword String password,
+            @ValidName String name
+    ) { }
+    public record Login(
+            @Email @NotBlank String email,
+            @ValidPassword String password
     ) { }
 }
