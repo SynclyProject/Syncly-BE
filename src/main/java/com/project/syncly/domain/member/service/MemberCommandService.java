58bed7ac5160502caea35a1100471c6011361f45
CHO <imalex12@naver.com>
2025-05-08 04:05:55 +0900

    feat: 소셜로그인 시 캐시 기능 추가
    소셜로그인 회원가입 시 캐싱. 일반 회원가입 시 이메일 인증여부 redis에서 확인기능 추가

--- src/main/java/com/project/syncly/domain/member/service/MemberCommandService.java
+++ src/main/java/com/project/syncly/domain/member/service/MemberCommandService.java
@@ -5,7 +5,7 @@ import com.project.syncly.domain.member.entity.Member;
 import com.project.syncly.domain.member.entity.SocialLoginProvider;
 
 public interface MemberCommandService {
-    public Member registerMember(MemberRequestDTO.signUp dto);
+    public Member registerMember(MemberRequestDTO.SignUp dto);
     public Member findOrCreateSocialMember(String email, String name, SocialLoginProvider provider);
 
 }
