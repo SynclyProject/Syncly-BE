7b2806289d46b109d52e5ee9cd86e259c9317fc8
강바다 <kbd1120@naver.com>
2025-05-07 21:44:16 +0900

    :sparkles: feat: Swagger 설명 추가

--- src/main/java/com/project/syncly/domain/workspace/controller/WorkspaceController.java
+++ src/main/java/com/project/syncly/domain/workspace/controller/WorkspaceController.java
@@ -4,6 +4,8 @@ import com.project.syncly.domain.workspace.service.WorkspaceServiceImpl;
 import com.project.syncly.domain.workspace.dto.WorkspaceRequestDto;
 import com.project.syncly.domain.workspace.dto.WorkspaceResponseDto;
 import com.project.syncly.global.apiPayload.CustomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,12 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/workspaces")
+@Tag(name = "WorkSpace 관련 API")
 public class WorkspaceController {
 
     private final WorkspaceServiceImpl workspaceService;
 
     // 개인 워크스페이스 생성 API
     @PostMapping("/personal")
+    @Operation(summary = "개인 워크 스페이스 생성 API")
     public ResponseEntity<CustomResponse<WorkspaceResponseDto.CreateWorkspaceResponseDto>> createPersonalWorkspace(
             //@AuthenticationPrincipal CustomUserDetails userDetails
     ) {
@@ -36,6 +40,7 @@ public class WorkspaceController {
 
     // 팀 워크스페이스 생성 API
     @PostMapping("/team")
+    @Operation(summary = "팀 워크 스페이스 생성 API")
     public ResponseEntity<CustomResponse<WorkspaceResponseDto.CreateWorkspaceResponseDto>> createTeamWorkspace(
             //@AuthenticationPrincipal CustomUserDetails userDetails
             @RequestBody @Valid WorkspaceRequestDto.CreateTeamWorkspaceRequestDto workspaceRequestDto
