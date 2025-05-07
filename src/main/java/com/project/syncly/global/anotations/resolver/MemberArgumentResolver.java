827c26f15fa886672252a09cce6e2e7afb08ceb3
CHO <imalex12@naver.com>
2025-05-08 04:20:59 +0900

    feat: 메서드 변경
    redis에서 사용자 불러오는 메서드 사용으로 변경

--- src/main/java/com/project/syncly/global/anotations/resolver/MemberArgumentResolver.java
+++ src/main/java/com/project/syncly/global/anotations/resolver/MemberArgumentResolver.java
@@ -1,7 +1,7 @@
-package com.project.syncly.global.oauth.resolver;
+package com.project.syncly.global.anotations.resolver;
 
 import com.project.syncly.domain.member.entity.Member;
-import com.project.syncly.domain.member.repository.MemberRepository;
+import com.project.syncly.domain.member.service.MemberQueryService;
 import com.project.syncly.global.anotations.MemberInfo;
 import com.project.syncly.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +10,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.bind.support.WebDataBinderFactory;
 
 @Component
 @RequiredArgsConstructor
 public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final JwtProvider jwtProvider;
-    private final MemberRepository memberRepository;
+    private final MemberQueryService memberQueryService;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -25,13 +26,14 @@ public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-                                  NativeWebRequest webRequest, org.springframework.web.bind.support.WebDataBinderFactory binderFactory) throws Exception {
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 
         String token = webRequest.getHeader("Authorization");
         if (token != null && token.startsWith("Bearer ")) {
             token = token.substring(7);
-            Long memberId = jwtProvider.getMemberId(token);
-            return memberRepository.findById(memberId).orElse(null);
+
+            Long memberId = jwtProvider.getMemberIdWithBlacklistCheck(token);
+            return memberQueryService.getMemberById(memberId);
         }
         return null;
     }
