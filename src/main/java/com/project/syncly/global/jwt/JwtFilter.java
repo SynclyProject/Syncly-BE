47388557f59a2459d3a6df04bba5a10300ad12e9
CHO <imalex12@naver.com>
2025-05-08 04:01:48 +0900

    feat: 블랙리스트 검사 추가. 예외처리 수정
    블랙리스트 여부 검사 기능 추가. 예외 발생 시 JwtAuthenticationEntryPoint에 넘겨주어 client로 보낼 수 있도록 수정

--- src/main/java/com/project/syncly/global/jwt/JwtFilter.java
+++ src/main/java/com/project/syncly/global/jwt/JwtFilter.java
@@ -2,12 +2,10 @@ package com.project.syncly.global.jwt;
 
 import com.project.syncly.domain.member.exception.MemberErrorCode;
 import com.project.syncly.domain.member.exception.MemberException;
-import com.project.syncly.global.apiPayload.CustomResponse;
-import com.project.syncly.global.apiPayload.code.BaseErrorCode;
-import com.project.syncly.global.apiPayload.exception.CustomException;
 import com.project.syncly.global.jwt.exception.JwtErrorCode;
+import com.project.syncly.global.jwt.exception.JwtException;
 import com.project.syncly.global.jwt.service.TokenService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.syncly.domain.auth.blacklist.TokenBlacklistService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -34,48 +32,72 @@ public class JwtFilter extends OncePerRequestFilter {
     private final PrincipalDetailsService principalDetailsService;
     private final TokenService tokenService;
     private final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+    private final TokenBlacklistService tokenBlacklistService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        try {
-            String accessToken = extractAccessToken(request);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
-            if (StringUtils.hasText(accessToken)) {
-                if (jwtProvider.isValid(accessToken)) {
-                    processValidAccessToken(accessToken);
-                    filterChain.doFilter(request, response);
-                    return;
-                } else {
-                    request.setAttribute("exception", JwtErrorCode.INVALID_TOKEN);
+        String accessToken = extractAccessToken(request);
+
+        if (StringUtils.hasText(accessToken)) {
+            try {
+                jwtProvider.getClaims(accessToken); // 검증 실패 시 예외 발생
+                /// 블랙리스트 검사
+                if (tokenBlacklistService.isAccessTokenBlacklisted(accessToken)) {
+                    throw new JwtException(JwtErrorCode.BLACKLISTED_ACCESS_TOKEN);
                 }
-            } else {
-                request.setAttribute("exception", JwtErrorCode.EMPTY_TOKEN);
+
+                processValidAccessToken(accessToken);
+                filterChain.doFilter(request, response);//accessToken있다면 반환하고 JwtFilter 끝
+                return;
+            } catch (JwtException e) {
+                request.setAttribute("exception", e.getCode()); // 예외만 넘김
             }
-
-            Optional<Cookie> refreshTokenCookie = extractRefreshTokenCookie(request);
-            if (refreshTokenCookie.isPresent()) {
-                String refreshToken = refreshTokenCookie.get().getValue();
-
-                if (jwtProvider.isValid(refreshToken)) {
-                    String email = jwtProvider.getEmail(refreshToken);
-                    Long memberId = jwtProvider.getMemberId(refreshToken);
-                    UserDetails userDetails = principalDetailsService.loadUserByUsername(email);
-                    String newAccessToken = tokenService.generateNewAccessToken(memberId, email);
-                    response.setHeader("Authorization", "Bearer " + newAccessToken);
-                    Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                } else {
-                    tokenService.removeRefreshTokenCookie(response);
-                    request.setAttribute("exception", JwtErrorCode.EXPIRED_TOKEN);
-                }
-            }
-
-            filterChain.doFilter(request, response);
-        } catch (CustomException e) {
-            handleException(response, e);
+        } else {
+            request.setAttribute("exception", JwtErrorCode.EMPTY_TOKEN);
         }
+
+
+        Optional<Cookie> refreshTokenCookie = extractRefreshTokenCookie(request);
+        if (refreshTokenCookie.isPresent()) {
+            String refreshToken = refreshTokenCookie.get().getValue();
+            try {
+                jwtProvider.getClaims(refreshToken);
+                /// 블랙리스트 검사
+                if (tokenBlacklistService.isRefreshTokenBlacklisted(refreshToken)) {
+                    throw new JwtException(JwtErrorCode.BLACKLISTED_REFRESH_TOKEN);
+                }
+
+                String email = jwtProvider.getEmail(refreshToken);
+                Long memberId = jwtProvider.getMemberIdWithBlacklistCheck(refreshToken);
+                UserDetails userDetails = principalDetailsService.loadUserByUsername(email);
+
+                String newAccessToken = tokenService.reissueAccessToken(memberId, email, response);
+                response.setHeader("Authorization", "Bearer " + newAccessToken);
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                //재발급 성공했으면 예외 삭제
+                request.removeAttribute("exception");
+            } catch (JwtException e) {
+                tokenService.removeRefreshTokenCookie(response);
+
+                if (e.getCode() == JwtErrorCode.INVALID_TOKEN) {
+                    // 토큰 변조 가능성 있으므로 블랙리스트 추가
+                    tokenBlacklistService.blacklistRefreshToken(refreshToken);
+                }
+                request.setAttribute("exception", e.getCode());
+            }
+        }else {
+            // refresh token 자체가 없으면 예외 전달
+            request.setAttribute("exception", JwtErrorCode.EMPTY_TOKEN);
+        }
+
+        filterChain.doFilter(request, response); //무조건 마지막엔 호출
     }
-    
+
     private String extractAccessToken(HttpServletRequest request) {
         String header = request.getHeader("Authorization");
         if (StringUtils.hasText(header) && header.startsWith("Bearer ")) {
@@ -83,7 +105,7 @@ public class JwtFilter extends OncePerRequestFilter {
         }
         return null;
     }
-    
+
     private Optional<Cookie> extractRefreshTokenCookie(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
@@ -93,27 +115,17 @@ public class JwtFilter extends OncePerRequestFilter {
         }
         return Optional.empty();
     }
-    
+
     private void processValidAccessToken(String token) {
         String email = jwtProvider.getEmail(token);
         UserDetails userDetails = principalDetailsService.loadUserByUsername(email);
-        
+
         if (userDetails != null) {
             Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         } else {
+            // 이 부분만은 인증 실패로 간주되어야 하므로 EXCEPTION 설정
             throw new MemberException(MemberErrorCode.MEMBER_NOT_FOUND);
         }
-    }
-    
-    private void handleException(HttpServletResponse response, CustomException e) throws IOException {
-        BaseErrorCode code = e.getCode();
-        response.setStatus(code.getStatus().value());
-        response.setContentType("application/json; charset=UTF-8");
-
-        CustomResponse<Object> customResponse = CustomResponse.failure(code.getCode(),code.getMessage());
-
-        ObjectMapper om = new ObjectMapper();
-        om.writeValue(response.getOutputStream(), customResponse);
     }
 }
