5299684e97c306794e786c54068a95b7a4fd2fce
강바다 <kbd1120@naver.com>
2025-05-12 19:21:37 +0900

    :sparkles: feat: redis 설정 및 util 클래스 정의

---
+++ src/main/java/com/project/syncly/global/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.project.syncly.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory); // Redis 연결
+        redisTemplate.setKeySerializer(new StringRedisSerializer()); // Redis 키 직렬화
+        redisTemplate.setValueSerializer(new StringRedisSerializer()); // Redis 값 직렬화
+        return redisTemplate;
+    }
+
+}
+
