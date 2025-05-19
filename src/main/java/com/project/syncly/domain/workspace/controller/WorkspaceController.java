086f228afc8a328c2f7bb700a3e293e9d58e5cd3
강바다 <kbd1120@naver.com>
2025-05-19 15:56:41 +0900

    :recycle: refactor: memberId를 PrincipalDetails과 연결

--- src/main/java/com/project/syncly/domain/workspace/controller/WorkspaceController.java
+++ src/main/java/com/project/syncly/domain/workspace/controller/WorkspaceController.java
@@ -4,12 +4,15 @@ import com.project.syncly.domain.workspace.service.WorkspaceServiceImpl;
 import com.project.syncly.domain.workspace.dto.WorkspaceRequestDto;
 import com.project.syncly.domain.workspace.dto.WorkspaceResponseDto;
 import com.project.syncly.global.apiPayload.CustomResponse;
+import com.project.syncly.global.jwt.PrincipalDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,10 +29,9 @@ public class WorkspaceController {
     @PostMapping("/personal")
     @Operation(summary = "개인 워크 스페이스 생성 API")
     public ResponseEntity<CustomResponse<WorkspaceResponseDto.CreateWorkspaceResponseDto>> createPersonalWorkspace(
-            //@AuthenticationPrincipal CustomUserDetails userDetails
+            @AuthenticationPrincipal PrincipalDetails userDetails
     ) {
-        //Long memberId = userDetails.getId();
-        Long memberId = 1234L;
+        Long memberId = Long.valueOf(userDetails.getName());
 
         WorkspaceResponseDto.CreateWorkspaceResponseDto response = workspaceService.createPersonalWorkspace(memberId);
 
@@ -41,11 +43,10 @@ public class WorkspaceController {
     @PostMapping("/team")
     @Operation(summary = "팀 워크 스페이스 생성 API")
     public ResponseEntity<CustomResponse<WorkspaceResponseDto.CreateWorkspaceResponseDto>> createTeamWorkspace(
-            //@AuthenticationPrincipal CustomUserDetails userDetails
+            @AuthenticationPrincipal PrincipalDetails userDetails,
             @RequestBody @Valid WorkspaceRequestDto.CreateTeamWorkspaceRequestDto workspaceRequestDto
     ) {
-        //Long memberId = userDetails.getId();
-        Long memberId = 1234L;
+        Long memberId = Long.valueOf(userDetails.getName());
 
         WorkspaceResponseDto.CreateWorkspaceResponseDto response = workspaceService.createTeamWorkspace(memberId, workspaceRequestDto.workspaceName());
 
@@ -57,10 +58,10 @@ public class WorkspaceController {
     @Operation(summary = "워크스페이스 초대 API")
     public ResponseEntity<CustomResponse<WorkspaceResponseDto.InviteWorkspaceResponseDto>> inviteToWorkspace(
             @PathVariable("workspaceId") Long workspaceId,
-            @RequestBody @Valid WorkspaceRequestDto.CreateInvitationMailRequestDto workspaceRequestDto
+            @RequestBody @Valid WorkspaceRequestDto.CreateInvitationMailRequestDto workspaceRequestDto,
+            @AuthenticationPrincipal PrincipalDetails userDetails
     ) {
-        // Long inviterId = userDetails.getId(); // 실제 로그인 정보 사용 시
-        Long inviterId = 1234L; // 목 데이터
+        Long inviterId = Long.valueOf(userDetails.getName());
 
         WorkspaceResponseDto.InviteWorkspaceResponseDto response = workspaceService.inviteTeamWorkspace(workspaceId, inviterId, workspaceRequestDto.email());
 
@@ -71,10 +72,10 @@ public class WorkspaceController {
     @GetMapping("/accept/{token}")
     @Operation(summary = "워크스페이스 초대 수락 API(이메일 링크 클릭)")
     public ResponseEntity<CustomResponse<WorkspaceResponseDto.AcceptWorkspaceResponseDto>> acceptInvitationByToken(
+            @AuthenticationPrincipal PrincipalDetails userDetails,
             @PathVariable("token") String token
     ) {
-        // Long inviterId = userDetails.getId(); // 실제 로그인 정보 사용 시
-        Long inviteeId = 2L; // 목 데이터
+        Long inviteeId = Long.valueOf(userDetails.getName());
 
         WorkspaceResponseDto.AcceptWorkspaceResponseDto response = workspaceService.acceptInvitationByToken(inviteeId, token);
 
@@ -86,10 +87,10 @@ public class WorkspaceController {
     @PostMapping("/accept")
     @Operation(summary = "워크스페이스 초대 수락 API(알림창)")
     public ResponseEntity<CustomResponse<WorkspaceResponseDto.AcceptWorkspaceResponseDto>> acceptInvitation(
+            @AuthenticationPrincipal PrincipalDetails userDetails,
             @RequestBody @Valid WorkspaceRequestDto.acceptInvitationRequestDto workspaceRequestDto
     ) {
-        // Long inviterId = userDetails.getId(); // 실제 로그인 정보 사용 시
-        Long inviteeId = 3L; // 목 데이터
+        Long inviteeId = Long.valueOf(userDetails.getName());
 
         WorkspaceResponseDto.AcceptWorkspaceResponseDto response = workspaceService.acceptInvitation(inviteeId, workspaceRequestDto.invitationId());
 
@@ -100,10 +101,10 @@ public class WorkspaceController {
     @PostMapping("/reject")
     @Operation(summary = "워크스페이스 초대 거절 API(알림창)")
     public ResponseEntity<CustomResponse<WorkspaceResponseDto.RejectWorkspaceResponseDto>> rejectInvitation(
+            @AuthenticationPrincipal PrincipalDetails userDetails,
             @RequestBody @Valid WorkspaceRequestDto.rejectInvitationRequestDto workspaceRequestDto
     ) {
-        // Long inviterId = userDetails.getId(); // 실제 로그인 정보 사용 시
-        Long inviteeId = 2L; // 목 데이터
+        Long inviteeId = Long.valueOf(userDetails.getName());
 
         WorkspaceResponseDto.RejectWorkspaceResponseDto response = workspaceService.rejectInvitation(inviteeId, workspaceRequestDto.invitationId());
 
@@ -115,9 +116,9 @@ public class WorkspaceController {
     @GetMapping("/invites")
     @Operation(summary = "사용자의 초대 목록 조회")
     public ResponseEntity<CustomResponse<List<WorkspaceResponseDto.InvitationInfoDto>>> getInvitations(
+            @AuthenticationPrincipal PrincipalDetails userDetails
     ) {
-        // Long inviterId = userDetails.getId(); // 실제 로그인 정보 사용 시
-        Long memberId = 3L; // 목 데이터
+        Long memberId = Long.valueOf(userDetails.getName());
 
         List<WorkspaceResponseDto.InvitationInfoDto> invites = workspaceService.getInvitations(memberId);
 
