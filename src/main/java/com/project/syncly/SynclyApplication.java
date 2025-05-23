378a8332ef1bdb8badd6d88a8d444a084c99a29c
CHO <imalex12@naver.com>
2025-05-24 08:23:57 +0900

    fix: spring-cloud-aws-starter-s3 과 사용자정의 S3Client 충돌해결

--- src/main/java/com/project/syncly/SynclyApplication.java
+++ src/main/java/com/project/syncly/SynclyApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration.class
+        }
+)
 @EnableJpaAuditing
 public class SynclyApplication {
 
