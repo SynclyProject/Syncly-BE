caf2f9cdbf6eec1a34b77df36fd302e6da1787fe
CHO <imalex12@naver.com>
2025-05-24 07:50:02 +0900

    feat: s3버켓 pre-signed 등록기능 구현
    - url 생성, 사진삭제 등 직접 s3와 통신하는 로직은 S3Util에서 구현

---
+++ src/main/java/com/project/syncly/domain/s3/util/S3Util.java
@@ -0,0 +1,68 @@
+package com.project.syncly.domain.s3.util;
+
+import com.project.syncly.domain.s3.exception.S3ErrorCode;
+import com.project.syncly.domain.s3.exception.S3Exception;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.net.URL;
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class S3Util {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final S3Presigner s3Presigner;
+    private final S3Client s3Client;
+
+    public String createPresignedUrl(String objectKey, String mimeType) {
+        try {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(objectKey)
+                    .contentType(mimeType)
+                    .build();
+
+            PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                    .putObjectRequest(putObjectRequest)
+                    .signatureDuration(Duration.ofMinutes(5))
+                    .build();
+
+            PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
+            return presignedRequest.url().toString();
+        } catch (Exception e) {
+            throw new S3Exception(S3ErrorCode.PRESIGNED_URL_FAILED);
+        }
+    }
+
+    public void delete(String objectKeyOrUrl) {
+        try {
+            String objectKey = objectKeyOrUrl.startsWith("https://")
+                    ? objectKeyOrUrl.replace("https://" + bucket + ".s3.amazonaws.com/", "")
+                    : objectKeyOrUrl;
+
+            DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(objectKey)
+                    .build();
+
+            s3Client.deleteObject(deleteRequest);
+        } catch (Exception e) {
+            throw new S3Exception(S3ErrorCode.DELETE_FAILED);
+        }
+    }
+
+    public String getObjectUrl(String objectKey) {
+        return "https://" + bucket + ".s3.amazonaws.com/" + objectKey;
+    }
+}
