73794be08f0f21444fcaa691235ca71512dd7822
CHO <imalex12@naver.com>
2025-05-25 21:31:43 +0900

    refactor: 파일 확장자와 mime 일치여부 검사 추가
    - fromExtension() : 확장자를 FileMimeType으로 변경
    - fromMimeType() : String을 FileMimeType으로 변경
    - isFileNameAndMimeTypeMatch() : 파일 확장자와 mime 일치여부 검사

--- src/main/java/com/project/syncly/domain/s3/enums/FileMimeType.java
+++ src/main/java/com/project/syncly/domain/s3/enums/FileMimeType.java
@@ -1,5 +1,7 @@
 package com.project.syncly.domain.s3.enums;
 
+import com.project.syncly.domain.s3.exception.S3Exception;
+import com.project.syncly.domain.s3.exception.S3ErrorCode;
 import lombok.Getter;
 
 import java.util.Arrays;
@@ -20,6 +22,23 @@ public enum FileMimeType {
     public static boolean isValid(String input) {
         return Arrays.stream(values())
                 .anyMatch(e -> e.mimeType.equalsIgnoreCase(input));
+    }
+
+    // 확장자 -> mime
+    public static FileMimeType fromExtension(String ext) {
+        return switch (ext.toLowerCase()) {
+            case "jpg" -> JPG;
+            case "jpeg" -> JPEG;
+            case "png" -> PNG;
+            default -> throw new S3Exception(S3ErrorCode.UNSUPPORTED_FILE_EXTENSION);
+        };
+    }
+
+    public static FileMimeType fromMimeType(String mimeType) {
+        return Arrays.stream(values())
+                .filter(e -> e.mimeType.equalsIgnoreCase(mimeType))
+                .findFirst()
+                .orElseThrow(() -> new S3Exception(S3ErrorCode.UNSUPPORTED_MIME_TYPE));
     }
 }
 
