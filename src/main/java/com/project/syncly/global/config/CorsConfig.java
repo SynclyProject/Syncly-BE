fffa91c0195d98458707a1d966eb8e5e9fa75b75
CHO <imalex12@naver.com>
2025-08-20 15:14:59 +0900

    refactor : SecurityConfig 단계 분리 및 cors 전략 분리

--- src/main/java/com/project/syncly/global/config/CorsConfig.java
+++ src/main/java/com/project/syncly/global/config/CorsConfig.java
@@ -6,27 +6,47 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig {//cross-origin 응답을 JS에서 읽을 수 있는지 통제
+    private static final List<String> PROD_ORIGINS = List.of(
+            "http://localhost:3000",
+            "http://127.0.0.1:5500",
+            "http://localhost:5173",// react dev,
+            "https://syncly-io.com"
+    );
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "http://127.0.0.1:5500",
-                "http://localhost:5173"// react dev
-        )); // 프론트엔드 서버
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.setAllowCredentials(true); // 쿠키를 포함한 요청 허용
-        configuration.setMaxAge(3600L);// 요청 결과 캐싱 시간 (1시간)
-        configuration.addExposedHeader("Authorization");//헤더 접근 허용
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);//내 도메인으로 제한
+
+        //쿠키 허용 api
+        // 크리덴셜 필요한 경로들
+        List<String> CRED_PATHS = List.of(
+                "/api/auth/**",
+                "/api/livekit/**",
+                "/api/s3/view-cookie"
+        );
+        CorsConfiguration cred = new CorsConfiguration();
+        cred.setAllowedOrigins(PROD_ORIGINS);
+        cred.setAllowCredentials(true);// 쿠키를 포함한 요청 허용
+        cred.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
+        cred.setAllowedHeaders(List.of("Authorization","Content-Type" ,"Accept", "Origin", "Referer", "X-Requested-With"));
+        cred.setMaxAge(3600L);
+        for (String path : CRED_PATHS) {
+            source.registerCorsConfiguration(path, cred);
+        }
+
+        //쿠키 비 허용
+        CorsConfiguration nonCred = new CorsConfiguration();
+        nonCred.setAllowedOrigins(PROD_ORIGINS);
+        nonCred.setAllowCredentials(false);// 쿠키를 포함한 요청 거부
+        nonCred.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
+        nonCred.setAllowedHeaders(List.of("Authorization","Content-Type" ,"Accept","Origin","X-Requested-With"));
+        nonCred.setMaxAge(3600L);
+        source.registerCorsConfiguration("/api/**", nonCred);
+
         return source;
     }
 }
