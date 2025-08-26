40d2bc7edbda37db2484d3133e052825845070ea
CHO <imalex12@naver.com>
2025-08-26 20:47:14 +0900

    fix : 스웨거 401오류 해결 시도

--- src/main/java/com/project/syncly/global/config/SwaggerConfig.java
+++ src/main/java/com/project/syncly/global/config/SwaggerConfig.java
@@ -14,19 +14,26 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI swagger() {
-        Info info = new Info().title("Syncly").description("공동 URL 작업, 파일공유, 화면공유 협업 프로젝트").version("0.0.1");
-        String securityScheme = "JWT TOKEN";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+        Info info = new Info()
+                .title("Syncly")
+                .description("공동 URL 작업, 파일공유, 화면공유 협업 프로젝트")
+                .version("0.0.1");
+
+        final String securitySchemeName = "bearerAuth"; // 공백 없는 단순한 이름 권장
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+
         Components components = new Components()
-                .addSecuritySchemes(securityScheme, new SecurityScheme()
-                        .name(securityScheme)
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName)
                         .type(SecurityScheme.Type.HTTP)
-                        .scheme("Bearer")
+                        .scheme("bearer")        // :white_check_mark: 반드시 소문자
                         .bearerFormat("JWT"));
+
         return new OpenAPI()
                 .info(info)
                 .addServersItem(new Server().url("/"))
-                .addSecurityItem(securityRequirement)
+                .addSecurityItem(securityRequirement) // 글로벌 적용
                 .components(components);
     }
 }
